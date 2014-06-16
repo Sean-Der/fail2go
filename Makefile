757d17d2e09beade7d1e7b2bc2f39e26a819cfc9
Sean DuBois <sean@siobud.com>
2014-06-16 08:17:32 +0000

    Add Makefile and implement JailFailRegex

---
+++ Makefile
@@ -0,0 +1,4 @@
+default: libs
+
+libs:
+	go get github.com/kisielk/og-rek
--- jail.go
+++ jail.go
@@ -12,7 +12,7 @@ func JailStatus(jail string) (map[string]interface{}, error) {
 		return nil, err
 	}
 
-	//	//TODO use reflection to assert data structures and give proper errors
+	//TODO use reflection to assert data structures and give proper errors
 	action := fail2banOutput.([]interface{})[1].([]interface{})[1].([]interface{})[1]
 	filter := fail2banOutput.([]interface{})[1].([]interface{})[0].([]interface{})[1]
 
@@ -27,4 +27,20 @@ func JailStatus(jail string) (map[string]interface{}, error) {
 	output["ipList"] = action.([]interface{})[2].([]interface{})[1]
 
 	return output, nil
+}
+
+func JailFailRegex(jail string) (map[string][]interface{}, error) {
+	fail2banInput := make([]string, 3)
+	fail2banInput[0] = "get"
+	fail2banInput[1] = jail
+	fail2banInput[2] = "failregex"
+
+	fail2banOutput, err := fail2banRequest(fail2banInput)
+	if err != nil {
+		return nil, err
+	}
+
+	return map[string][]interface{}{
+		"failregex": fail2banOutput.([]interface{})[1].([]interface{}),
+	}, nil
 }
